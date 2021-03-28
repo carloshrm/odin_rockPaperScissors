39e63eb50780240e65d265ee6c4d85cff2f4d1cc
chm <carloshrmoraes@gmail.com>
2021-03-28 15:00:53 -0300

    add html

--- rps.js
+++ rps.js
@@ -1,12 +1,26 @@
 let result;
 let playerChoice;
 let computerChoice;
+let round = 0;
+let score = 0;
+let roundChoice = 0;
 const win = ", you won!";
 const loss = ", you lost...";
 
-function playRound(playerChoice, computerChoice) {
+const textDisplay = document.querySelector("#results");
+const textFeedback = document.querySelector(".text");
+const dropdown = document.querySelector("#roundSelect");
+const start = document.querySelector("#startButton");
+const buttons = document.querySelectorAll("#button_container > button");
+
+buttons.forEach((btn) => btn.addEventListener("click", setPlayerChoice));
+buttons.forEach((btn) => btn.addEventListener("click", game));
+start.addEventListener("click", startGame);
+window.addEventListener("DOMContentLoaded", initialSetup);
+
+function evaluateChoices() {
   if (playerChoice === computerChoice) {
-    return `The computer chose ${computerChoice}, it's a tie.`;
+    textFeedBack.textContent = `The computer chose ${computerChoice}, it's a tie.`;
   }
 
   if (playerChoice === "rock") {
@@ -32,27 +46,75 @@ function playRound(playerChoice, computerChoice) {
       result = loss;
     }
   }
-  return `The computer chose ${computerChoice}${result}`;
+  if (result === win) score++;
+}
+
+function playRound() {
+  round++;
+  evaluateChoices();
+
+  resultsText.textContent = `It's round: ${round}, you have won ${score} and lost ${
+    round - score
+  }.`;
+  textFeedBack.textContent = `The computer chose ${computerChoice}${result}`;
+
+  if (round === +roundChoice) {
+    endGame();
+  }
+  return;
 }
 
 function computerPlay() {
   let random = Math.floor(Math.random() * Math.floor(3));
-  if (random === 1) return "rock";
-  if (random === 0) return "paper";
-  if (random === 2) return "scissors";
-}
-
-function playerSelection() {
-  let rawInput = prompt("Type in a choice: rock, paper, or scissors...");
-  rawInput = rawInput.match(/(rock|paper|scissors)/i);
-  try {
-    return rawInput[0];
-  } catch {
-    alert("Not a valid choice, try again.");
-    return;
-  }
+  if (random === 1) computerChoice = "rock";
+  if (random === 0) computerChoice = "paper";
+  if (random === 2) computerChoice = "scissors";
 }
 
 function game() {
-  console.log(playRound(playerSelection(), computerPlay()));
+  computerPlay();
+  playRound();
+}
+
+function startGame(e) {
+  roundChoice = dropdown.value;
+  dropdown.style.visibility = "collapse";
+  start.style.visibility = "collapse";
+  resultsText.textContent = " ... ";
+  buttons.forEach((btn) => (btn.style.visibility = "visible"));
+  textFeedback.style.visibility = "visible";
+}
+
+function endGame() {
+  buttons.forEach((btn) => (btn.style.visibility = "hidden"));
+  if (score === round - score) {
+    resultsText.textContent = `The match was a tie, you won ${score} against ${
+      round - score
+    }.`;
+    return;
+  }
+  if (score > round - score) {
+    resultsText.textContent = `You won the match with a score of ${score}!`;
+  } else {
+    resultsText.textContent = `You lost the match with a score of ${score} against ${
+      round - score
+    }...`;
+  }
+  start.textContent = "Restart";
+  start.style.visibility = "visible";
+  start.removeEventListener("click", startGame);
+  start.addEventListener("click", initialSetup);
+}
+function setPlayerChoice(e) {
+  playerChoice = e.srcElement.id;
+}
+function initialSetup() {
+  resultsText.textContent = "Choose a number of rounds: ";
+  start.addEventListener("click", startGame);
+  start.removeEventListener("click", initialSetup);
+  textFeedback.style.visibility = "hidden";
+  dropdown.style.visibility = "visible";
+  start.textContent = "Start!";
+  round = 0;
+  score = 0;
 }
